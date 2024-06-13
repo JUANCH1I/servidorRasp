ca125fda8984bce61f0f8c73b734111e2ae90629
JUNACH1I <juanimanfredi@gmail.com>
2024-06-13 12:03:07 -0500

    public

--- index.js
+++ index.js
@@ -1,38 +1,42 @@
-// servidor/index.js
 const express = require('express')
 const http = require('http')
-const path = require('path')
 const socketIo = require('socket.io')
+const path = require('path')
 
 const app = express()
 const server = http.createServer(app)
 const io = socketIo(server)
 
-// Middleware para servir archivos estáticos del frontend
-app.use(express.static(path.join(__dirname, '../frontend/build')))
+let clients = []
+
+// Servir archivos estáticos del frontend
+app.use(express.static(path.join(__dirname, 'public')))
 
 io.on('connection', (socket) => {
-  console.log('Nueva Raspberry conectada')
+  console.log('Nueva conexión: ', socket.id)
+  clients.push(socket.id)
 
+  // Reenviar video a todos los clientes
   socket.on('video', (data) => {
-    // Transmitir el video a todos los clientes conectados
     socket.broadcast.emit('video', data)
   })
 
+  // Reenviar audio a todos los clientes
+  socket.on('audio', (data) => {
+    socket.broadcast.emit('audio', data)
+  })
+
+  // Controlar pines GPIO
   socket.on('control', (command) => {
-    // Manejar comandos para los pines GPIO
+    io.emit('control', command)
   })
 
   socket.on('disconnect', () => {
-    console.log('Raspberry desconectada')
+    console.log('Desconexión: ', socket.id)
+    clients = clients.filter((id) => id !== socket.id)
   })
 })
 
-// Ruta para servir la aplicación frontend
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'))
-})
-
-server.listen(1234, () => {
+server.listen(3000, () => {
   console.log('Servidor corriendo en el puerto 3000')
 })
