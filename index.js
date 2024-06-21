4270b1a2cf96d0a372e07199719199e5a8e661ef
JUNACH1I <juanimanfredi@gmail.com>
2024-06-21 13:36:37 -0500

    change json and front

--- index.js
+++ index.js
@@ -1,27 +1,46 @@
 const express = require('express')
 const axios = require('axios')
+const fs = require('node:fs')
 const path = require('path')
 const app = express()
 const port = process.env.PORT || 3000
 
-let raspberries = {}
+const raspberriesFilePath = 'raspberries.json'
 
 app.use(express.json())
+
+// Leer la lista de Raspberry Pis desde el archivo
+const loadRaspberries = () => {
+  if (fs.existsSync(raspberriesFilePath)) {
+    const data = fs.readFileSync(raspberriesFilePath)
+    return JSON.parse(data)
+  }
+  return {}
+}
+
+// Guardar la lista de Raspberry Pis en el archivo
+const saveRaspberries = (raspberries) => {
+  fs.writeFileSync(raspberriesFilePath, JSON.stringify(raspberries, null, 2))
+}
 
 // Endpoint para registrar las Raspberry Pis
 app.post('/register', (req, res) => {
   const { id, ip } = req.body
+  const raspberries = loadRaspberries()
   raspberries[id] = ip
+  saveRaspberries(raspberries)
   console.log(`Raspberry Pi ${id} registrada con IP ${ip}`)
   res.send('Raspberry Pi registrada')
 })
 
 // Endpoint para obtener la lista de Raspberry Pis registradas
 app.get('/raspberries', (req, res) => {
+  const raspberries = loadRaspberries()
   const raspberryList = Object.keys(raspberries).map((id) => ({
     id,
     ip: raspberries[id],
   }))
+  console.log('Enviando lista de raspberries:', raspberryList)
   res.json(raspberryList)
 })
 
@@ -31,18 +50,23 @@ app.get('/gpio/:id/:pin/:state', async (req, res) => {
   const pin = req.params.pin
   const state = req.params.state
 
+  const raspberries = loadRaspberries()
   const ip = raspberries[id]
   if (!ip) {
+    console.log(`Raspberry Pi ${id} no encontrada`)
     return res.status(404).send('Raspberry Pi no encontrada')
   }
 
   try {
+    console.log(
+      `Enviando solicitud a Raspberry Pi ${id} en ${ip} para pin ${pin} ${state}`
+    )
     const response = await axios.get(`http://${ip}:5000/gpio/${pin}/${state}`)
     res.send(response.data)
   } catch (error) {
     console.error(
       `Error al comunicarse con la Raspberry Pi ${id} en ${ip}:`,
-      error
+      error.message
     )
     res.status(500).send('Error al comunicarse con la Raspberry Pi')
   }
