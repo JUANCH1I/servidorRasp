7c87f44ad8784969ba31592d68b6fd63272a768c
JUNACH1I <juanimanfredi@gmail.com>
2024-06-21 12:41:44 -0500

    logs

--- index.js
+++ index.js
@@ -2,17 +2,16 @@ const express = require('express')
 const axios = require('axios')
 const path = require('path')
 const app = express()
-const cors = require('cors')
-const port = 3000
+const port = process.env.PORT || 3000
 
-const raspberries = {}
+let raspberries = {}
 
 app.use(express.json())
-app.use(cors())
 
 app.post('/register', (req, res) => {
   const { id, ip } = req.body
   raspberries[id] = ip
+  console.log(`Raspberry Pi ${id} registrada con IP ${ip}`)
   res.send('Raspberry Pi registrada')
 })
 
@@ -31,13 +30,21 @@ app.get('/gpio/:id/:pin/:state', async (req, res) => {
 
   const ip = raspberries[id]
   if (!ip) {
+    console.log(`Raspberry Pi ${id} no encontrada`)
     return res.status(404).send('Raspberry Pi no encontrada')
   }
 
   try {
     const response = await axios.get(`http://${ip}:5000/gpio/${pin}/${state}`)
+    console.log(
+      `Comando enviado a Raspberry Pi ${id} en ${ip}: pin ${pin} ${state}`
+    )
     res.send(response.data)
   } catch (error) {
+    console.error(
+      `Error al comunicarse con la Raspberry Pi ${id} en ${ip}:`,
+      error
+    )
     res.status(500).send('Error al comunicarse con la Raspberry Pi')
   }
 })
