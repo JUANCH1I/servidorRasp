53471fb7e5ccb78e48d297dd3be91e986a1fb430
JUNACH1I <juanimanfredi@gmail.com>
2024-06-25 12:13:38 -0500

    simple peer

--- index.js
+++ index.js
@@ -1,53 +1,45 @@
-const express = require('express')
 const http = require('http')
-const WebSocket = require('ws')
-const path = require('path')
+const express = require('express')
+const socketIo = require('socket.io')
+const Peer = require('simple-peer')
+const wrtc = require('wrtc')
+
 const app = express()
-const port = process.env.PORT || 3000
-
-app.use(express.json())
-
 const server = http.createServer(app)
-const wss = new WebSocket.Server({ server })
+const io = socketIo(server)
 
-let clients = []
+app.use(express.static('public'))
 
-wss.on('connection', (ws, req) => {
-  clients.push(ws)
+io.on('connection', (socket) => {
+  console.log('Nuevo cliente conectado')
 
-  ws.on('message', (message) => {
-    // Reenviar el mensaje de video a todos los clientes conectados
-    clients.forEach((client) => {
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(message)
-      }
+  socket.on('start-stream', () => {
+    const peer = new Peer({ initiator: false, wrtc })
+
+    peer.on('signal', (data) => {
+      socket.emit('signal', data)
+    })
+
+    socket.on('signal', (data) => {
+      peer.signal(data)
+    })
+
+    peer.on('data', (data) => {
+      socket.emit('video-data', data)
     })
   })
 
-  ws.on('close', () => {
-    clients = clients.filter((client) => client !== ws)
+  socket.on('control', (msg) => {
+    // Reenviar control de GPIO a la Raspberry Pi
+    const { device, state } = msg
+    socket.broadcast.emit('control', { device, state })
+  })
+
+  socket.on('disconnect', () => {
+    console.log('Cliente desconectado')
   })
 })
 
-app.post('/api/raspberry/:id/gpio', (req, res) => {
-  const { id } = req.params
-  const { device, state } = req.body
-  const instruction = JSON.stringify({ device, state })
-  clients.forEach((client) => {
-    client.send(instruction)
-  })
-  res
-    .status(200)
-    .send(
-      `GPIO del dispositivo ${device} en Raspberry Pi ${id} configurado a ${state}`
-    )
-})
-
-// Servir el archivo HTML
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'))
-})
-
-server.listen(port, () => {
-  console.log(`Servidor de API corriendo en http://localhost:${port}`)
+server.listen(3000, () => {
+  console.log('Servidor escuchando en puerto 3000')
 })
